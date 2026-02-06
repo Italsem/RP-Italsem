1d4609ad4942349242b5732e9ee7d6f3b1e59ff3
GESTIONE FLOTTE <gestioneflotteitalsem@gmail.com>
2026-02-06 12:26:25 +0100

    Add tailwind to fix PostCSS build on Pages

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "brand-orange": "#ff6a00"
+      }
+    }
+  },
+  plugins: []
+};
